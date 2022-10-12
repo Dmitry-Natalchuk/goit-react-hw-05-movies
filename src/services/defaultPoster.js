5d1450832e5c88cfeda5017e9ac689f9cdeb0106
Dmitry Natalchuk <next1996@ukr.net>
2022-10-12 17:53:34 +0300

    fix defaultPoster

--- src/services/defaultPoster.js
+++ src/services/defaultPoster.js
@@ -1,5 +1,5 @@
 const DEFAULT_IMG =
-  'https://icon-library.com/icon/not-found-icon-4.html.html>Not Found Icon # 416177';
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNIZxcqjHSXy3woDzxKGajAzwWnoc96DXFgOFxhb0r5rCn61zO&s';
 const BASE_IMG = 'https://image.tmdb.org/t/p/w500';
 
 export const getPosterImg = path =>
