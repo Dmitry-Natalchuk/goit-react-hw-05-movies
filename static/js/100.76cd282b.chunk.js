b8d83bf6252ab40561008cd0ebb98fbbc3c3c7e3
Dmitry-Natalchuk <95769351+Dmitry-Natalchuk@users.noreply.github.com>
2022-10-13 17:34:21 +0000

    Deploying to gh-pages from @ Dmitry-Natalchuk/goit-react-hw-05-movies@6db00d0088a906a9d83e94a342053d6596d1d596 🚀

--- static/js/100.76cd282b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[100],{100:function(e,r,t){t.r(r);var l=t(731),s=t(184);r.default=function(){return(0,s.jsxs)("ul",{children:[(0,s.jsx)("li",{children:(0,s.jsx)(l.OL,{to:"/",children:"Home"})}),(0,s.jsx)("li",{children:(0,s.jsx)(l.OL,{to:"/movies",children:"Movies"})})]})}}}]);
-//# sourceMappingURL=100.76cd282b.chunk.js.map
